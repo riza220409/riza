5e51a43acc7c9a5b4b793ba8ed4aacd90c1ff597
Ay Gemuy 24 <91945119+AyGemuy@users.noreply.github.com>
2021-12-06 20:47:07 +0800

    SelfBot
    SelfBot

---
+++ list/list.js
@@ -0,0 +1,479 @@
+const tpro_list = (prefix)=> {
+return`⋆⸙ *TEXT PRO LIST* ⸙⋆
+❉▰▰▱▱▣▣▱▱▰▰❉
+✗⃝🕊 ️${prefix}3dbox _text_
+✗⃝🕊 ️${prefix}3dchrome _text_
+✗⃝🕊 ️${prefix}3dglowing _text_
+✗⃝🕊 ️${prefix}3dglue _text_
+✗⃝🕊 ️${prefix}3dneonlight _text_
+✗⃝🕊 ️${prefix}3dstone _text_
+✗⃝🕊 ️${prefix}3dunderwater _text_
+✗⃝🕊 ️${prefix}abstra _text_
+✗⃝🕊 ️${prefix}abstragold _text_
+✗⃝🕊 ️${prefix}advanced _text_
+✗⃝🕊 ️${prefix}advancedglow _text_
+✗⃝🕊 ️${prefix}artpapercut _text_
+✗⃝🕊 ️${prefix}balloon _text_
+✗⃝🕊 ️${prefix}balloon2 _text_
+✗⃝🕊 ️${prefix}balloon3 _text_
+✗⃝🕊 ️${prefix}balloon4 _text_
+✗⃝🕊 ️${prefix}balloon5 _text_
+✗⃝🕊 ️${prefix}balloon6 _text_
+✗⃝🕊 ️${prefix}balloon7 _text_
+✗⃝🕊 ️${prefix}bear _text_
+✗⃝🕊 ️${prefix}bearlogo _text_
+✗⃝🕊 ️${prefix}berry _text_
+✗⃝🕊 ️${prefix}biscuit _text_
+✗⃝🕊 ️${prefix}blackpink _text_
+✗⃝🕊 ️${prefix}blood _text_
+✗⃝🕊 ️${prefix}blood2 _text_
+✗⃝🕊 ️${prefix}bloodtext _text_
+✗⃝🕊 ️${prefix}bluegem _text_
+✗⃝🕊 ️${prefix}bluemetal _text_
+✗⃝🕊 ️${prefix}bokeh _text_
+✗⃝🕊 ️${prefix}bokehtext _text_
+✗⃝🕊 ️${prefix}bread _text_
+✗⃝🕊 ️${prefix}breakwall _text_
+✗⃝🕊 ️${prefix}broken _text_
+✗⃝🕊 ️${prefix}brokenglass _text_
+✗⃝🕊 ️${prefix}bronzeglitter _text_
+✗⃝🕊 ️${prefix}candy _text_
+✗⃝🕊 ️${prefix}captainamerica _text_
+✗⃝🕊 ️${prefix}captain_as2 _text_
+✗⃝🕊 ️${prefix}captamerica _text_
+✗⃝🕊 ️${prefix}carbon _text_
+✗⃝🕊 ️${prefix}chocolate _text_
+✗⃝🕊 ️${prefix}chocolatecake _text_
+✗⃝🕊 ️${prefix}chrismast _text_
+✗⃝🕊 ️${prefix}christmas _text_
+✗⃝🕊 ️${prefix}cloud _text_
+✗⃝🕊 ️${prefix}cloud2 _text_
+✗⃝🕊 ️${prefix}cloudsky _text_
+✗⃝🕊 ️${prefix}darkg _text_
+✗⃝🕊 ️${prefix}decorate _text_
+✗⃝🕊 ️${prefix}decorate2 _text_
+✗⃝🕊 ️${prefix}decorategreen _text_
+✗⃝🕊 ️${prefix}decorative _text_
+✗⃝🕊 ️${prefix}deluxe2 _text_
+✗⃝🕊 ️${prefix}deluxesilver _text_
+✗⃝🕊 ️${prefix}demon _text_
+✗⃝🕊 ️${prefix}denim _text_
+✗⃝🕊 ️${prefix}discovery _text_
+✗⃝🕊 ️${prefix}dropwater _text_
+✗⃝🕊 ️${prefix}drug _text_
+✗⃝🕊 ️${prefix}embossed _text_
+✗⃝🕊 ️${prefix}engraved _text_
+✗⃝🕊 ️${prefix}engraved3d _text_
+✗⃝🕊 ️${prefix}equalizer _text_
+✗⃝🕊 ️${prefix}erodedmetal _text_
+✗⃝🕊 ️${prefix}fabric _text_
+✗⃝🕊 ️${prefix}fiction _text_
+✗⃝🕊 ️${prefix}firework _text_
+✗⃝🕊 ️${prefix}fireworksparkle _text_
+✗⃝🕊 ️${prefix}foilballoon _text_
+✗⃝🕊 ️${prefix}fruitjuice _text_
+✗⃝🕊 ️${prefix}futuristic _text_
+✗⃝🕊 ️${prefix}gemb _text_
+✗⃝🕊 ️${prefix}glass _text_
+✗⃝🕊 ️${prefix}glass2 _text_
+✗⃝🕊 ️${prefix}glass3 _text_
+✗⃝🕊 ️${prefix}glass4 _text_
+✗⃝🕊 ️${prefix}glass5 _text_
+✗⃝🕊 ️${prefix}glass6 _text_
+✗⃝🕊 ️${prefix}glass7 _text_
+✗⃝🕊 ️${prefix}glass8 _text_
+✗⃝🕊 ️${prefix}glitch _text_
+✗⃝🕊 ️${prefix}glitter _text_
+✗⃝🕊 ️${prefix}glitter2 _text_
+✗⃝🕊 ️${prefix}glitter3 _text_
+✗⃝🕊 ️${prefix}glitter4 _text_
+✗⃝🕊 ️${prefix}glitter5 _text_
+✗⃝🕊 ️${prefix}glitter6 _text_
+✗⃝🕊 ️${prefix}glitter7 _text_
+✗⃝🕊 ️${prefix}gloss _text_
+✗⃝🕊 ️${prefix}glossy _text_
+✗⃝🕊 ️${prefix}glossyb _text_
+✗⃝🕊 ️${prefix}glossyc _text_
+✗⃝🕊 ️${prefix}glosycarbon _text_
+✗⃝🕊 ️${prefix}glue3d _text_
+✗⃝🕊 ️${prefix}gneon _text_
+✗⃝🕊 ️${prefix}golden _text_
+✗⃝🕊 ️${prefix}gradient _text_
+✗⃝🕊 ️${prefix}gradient2 _text_
+✗⃝🕊 ️${prefix}greenhorror _text_
+✗⃝🕊 ️${prefix}greenneon _text_
+✗⃝🕊 ️${prefix}halloween _text_
+✗⃝🕊 ️${prefix}harrypotter _text_
+✗⃝🕊 ️${prefix}harry_potter _text_
+✗⃝🕊 ️${prefix}holographic _text_
+✗⃝🕊 ️${prefix}honey _text_
+✗⃝🕊 ️${prefix}horror _text_
+✗⃝🕊 ️${prefix}horrorblood _text_
+✗⃝🕊 ️${prefix}ice _text_
+✗⃝🕊 ️${prefix}icecold _text_
+✗⃝🕊 ️${prefix}jewelry _text_
+✗⃝🕊 ️${prefix}jewelry2 _text_
+✗⃝🕊 ️${prefix}jewelry3 _text_
+✗⃝🕊 ️${prefix}jewelry4 _text_
+✗⃝🕊 ️${prefix}jewelry5 _text_
+✗⃝🕊 ️${prefix}jewelry6 _text_
+✗⃝🕊 ️${prefix}jewelry7 _text_
+✗⃝🕊 ️${prefix}jewelry8 _text_
+✗⃝🕊 ️${prefix}joker _text_
+✗⃝🕊 ️${prefix}juice _text_
+✗⃝🕊 ️${prefix}koifish _text_
+✗⃝🕊 ️${prefix}lava _text_
+✗⃝🕊 ️${prefix}lavatext _text_
+✗⃝🕊 ️${prefix}logojoker _text_
+✗⃝🕊 ️${prefix}luxury _text_
+✗⃝🕊 ️${prefix}luxurygold _text_
+✗⃝🕊 ️${prefix}magma _text_
+✗⃝🕊 ️${prefix}magmahot _text_
+✗⃝🕊 ️${prefix}marble _text_
+✗⃝🕊 ️${prefix}marble2 _text_
+✗⃝🕊 ️${prefix}matrix _text_
+✗⃝🕊 ️${prefix}matrixstyle _text_
+✗⃝🕊 ️${prefix}matrixtext _text_
+✗⃝🕊 ️${prefix}metalb _text_
+✗⃝🕊 ️${prefix}metalb2 _text_
+✗⃝🕊 ️${prefix}metaldark _text_
+✗⃝🕊 ️${prefix}metale _text_
+✗⃝🕊 ️${prefix}metalg _text_
+✗⃝🕊 ️${prefix}metalh _text_
+✗⃝🕊 ️${prefix}metallic _text_
+✗⃝🕊 ️${prefix}metalpurple _text_
+✗⃝🕊 ️${prefix}metalr _text_
+✗⃝🕊 ️${prefix}metalrainbow _text_
+✗⃝🕊 ️${prefix}metals _text_
+✗⃝🕊 ️${prefix}minion _text_
+✗⃝🕊 ️${prefix}miniontext _text_
+✗⃝🕊 ️${prefix}multicolor _text_
+✗⃝🕊 ️${prefix}natural _text_
+✗⃝🕊 ️${prefix}naturalleaves _text_
+✗⃝🕊 ️${prefix}neon _text_
+✗⃝🕊 ️${prefix}neon2 _text_
+✗⃝🕊 ️${prefix}neonc _text_
+✗⃝🕊 ️${prefix}neondevil _text_
+✗⃝🕊 ️${prefix}neonlight _text_
+✗⃝🕊 ️${prefix}neonlight2 _text_
+✗⃝🕊 ️${prefix}neonligth2 _text_
+✗⃝🕊 ️${prefix}neontext _text_
+✗⃝🕊 ️${prefix}neon_devil _text_
+✗⃝🕊 ️${prefix}neon_light _text_
+✗⃝🕊 ️${prefix}newyear _text_
+✗⃝🕊 ️${prefix}newyear2 _text_
+✗⃝🕊 ️${prefix}old1917 _text_
+✗⃝🕊 ️${prefix}oscar _text_
+✗⃝🕊 ️${prefix}papercut _text_
+✗⃝🕊 ️${prefix}peridot _text_
+✗⃝🕊 ️${prefix}pinksparkling _text_
+✗⃝🕊 ️${prefix}plasticbagdrug _text_
+✗⃝🕊 ️${prefix}pupleglass _text_
+✗⃝🕊 ️${prefix}purple _text_
+✗⃝🕊 ️${prefix}purpleg _text_
+✗⃝🕊 ️${prefix}purplegem _text_
+✗⃝🕊 ️${prefix}purpleshiny _text_
+✗⃝🕊 ️${prefix}rainbow _text_
+✗⃝🕊 ️${prefix}rainbowequalizer _text_
+✗⃝🕊 ️${prefix}realcloud _text_
+✗⃝🕊 ️${prefix}realvintage _text_
+✗⃝🕊 ️${prefix}roadw _text_
+✗⃝🕊 ️${prefix}roadwarning _text_
+✗⃝🕊 ️${prefix}robot _text_
+✗⃝🕊 ️${prefix}rock _text_
+✗⃝🕊 ️${prefix}rusty _text_
+✗⃝🕊 ️${prefix}rusymetal _text_
+✗⃝🕊 ️${prefix}sandengraved _text_
+✗⃝🕊 ️${prefix}sandsummer _text_
+✗⃝🕊 ️${prefix}sandsummery _text_
+✗⃝🕊 ️${prefix}sandwrite _text_
+✗⃝🕊 ️${prefix}scifi _text_
+✗⃝🕊 ️${prefix}shiny _text_
+✗⃝🕊 ️${prefix}shinymetal _text_
+✗⃝🕊 ️${prefix}sircuit _text_
+✗⃝🕊 ️${prefix}skeleton _text_
+✗⃝🕊 ️${prefix}sketch _text_
+✗⃝🕊 ️${prefix}snow _text_
+✗⃝🕊 ️${prefix}sparkling _text_
+✗⃝🕊 ️${prefix}sparkling2 _text_
+✗⃝🕊 ️${prefix}sparkling3 _text_
+✗⃝🕊 ️${prefix}sparkling4 _text_
+✗⃝🕊 ️${prefix}sparkling5 _text_
+✗⃝🕊 ️${prefix}sparkling6 _text_
+✗⃝🕊 ️${prefix}sparkling7 _text_
+✗⃝🕊 ️${prefix}steel _text_
+✗⃝🕊 ️${prefix}strawberry _text_
+✗⃝🕊 ️${prefix}summer _text_
+✗⃝🕊 ️${prefix}summerbeach _text_
+✗⃝🕊 ️${prefix}summery _text_
+✗⃝🕊 ️${prefix}text3d _text_
+✗⃝🕊 ️${prefix}thunder _text_
+✗⃝🕊 ️${prefix}thunder2 _text_
+✗⃝🕊 ️${prefix}thundertext _text_
+✗⃝🕊 ️${prefix}toxic _text_
+✗⃝🕊 ️${prefix}toxictext _text_
+✗⃝🕊 ️${prefix}transformer _text_
+✗⃝🕊 ️${prefix}ultragloss _text_
+✗⃝🕊 ️${prefix}underwater _text_
+✗⃝🕊 ️${prefix}wall _text_
+✗⃝🕊 ️${prefix}watercolor _text_
+✗⃝🕊 ️${prefix}water_pipe _text_
+✗⃝🕊 ️${prefix}wicker _text_
+✗⃝🕊 ️${prefix}wonderfulg _text_
+✗⃝🕊 ️${prefix}wonderfulgraffiti _text_
+✗⃝🕊 ️${prefix}wood _text_
+✗⃝🕊 ️${prefix}writing _text_
+✗⃝🕊 ️${prefix}xmas _text_
+✗⃝🕊 ️${prefix}xmascards _text_
+✗⃝🕊 ️${prefix}yellowglass _text_
+✗⃝🕊 ️${prefix}yellowjewelry _text_
+❉▰▰▱▱▣▣▱▱▰▰❉`
+}
+exports.tpro_list = tpro_list
+
+const epho_list = (prefix)=> {
+return`⋆⸙ *EPHOTO LIST* ⸙⋆
+❉▰▰▱▱▣▣▱▱▰▰❉
+✗⃝🕊 ️${prefix}3dcrack _text_
+✗⃝🕊 ️${prefix}3dhologram _text_
+✗⃝🕊 ️${prefix}3dshiny _text_
+✗⃝🕊 ️${prefix}3dunderwater _text_
+✗⃝🕊 ️${prefix}1917 _text_
+✗⃝🕊 ️${prefix}artpapercut _text_
+✗⃝🕊 ️${prefix}bearlogo _text_
+✗⃝🕊 ️${prefix}birthdaycake _text_
+✗⃝🕊 ️${prefix}blackpink _text_
+✗⃝🕊 ️${prefix}blackpinkneon _text_
+✗⃝🕊 ️${prefix}brokenglass _text_
+✗⃝🕊 ️${prefix}cake _text_
+✗⃝🕊 ️${prefix}cakes _text_
+✗⃝🕊 ️${prefix}cloud _text_
+✗⃝🕊 ️${prefix}clouds _text_
+✗⃝🕊 ️${prefix}galaxy _text_
+✗⃝🕊 ️${prefix}galaxybat _text_
+✗⃝🕊 ️${prefix}galaxystyle _text_
+✗⃝🕊 ️${prefix}glow _text_
+✗⃝🕊 ️${prefix}glowing _text_
+✗⃝🕊 ️${prefix}graffiti2 _text_
+✗⃝🕊 ️${prefix}graffiti3 _text_
+✗⃝🕊 ️${prefix}graffiti5 _text_
+✗⃝🕊 ️${prefix}graffiti _text_
+✗⃝🕊 ️${prefix}graffiticolor _text_
+✗⃝🕊 ️${prefix}greenbrush _text_
+✗⃝🕊 ️${prefix}hotmetalic _text_
+✗⃝🕊 ️${prefix}lighteffects _text_
+✗⃝🕊 ️${prefix}lightgalaxy _text_
+✗⃝🕊 ️${prefix}metal _text_
+✗⃝🕊 ️${prefix}neon2 _text_
+✗⃝🕊 ️${prefix}neon _text_
+✗⃝🕊 ️${prefix}neondevil _text_
+✗⃝🕊 ️${prefix}neonlight _text_
+✗⃝🕊 ️${prefix}pubgmascot _text_
+✗⃝🕊 ️${prefix}snake _text_
+✗⃝🕊 ️${prefix}startsnight2 _text_
+✗⃝🕊 ️${prefix}startsnight _text_
+✗⃝🕊 ️${prefix}summerbeach2 _text_
+✗⃝🕊 ️${prefix}summerbeach _text_
+✗⃝🕊 ️${prefix}thunder _text_
+✗⃝🕊 ️${prefix}watercolor _text_
+✗⃝🕊 ️${prefix}wetglass _text_
+✗⃝🕊 ️${prefix}wooden3d _text_
+✗⃝🕊 ️${prefix}wordgreen _text_
+✗⃝🕊 ️${prefix}writingchalk _text_
+✗⃝🕊 ️${prefix}zombie3d _text_
+❉▰▰▱▱▣▣▱▱▰▰❉`
+}
+exports.epho_list = epho_list
+
+const pfun_list = (prefix)=> {
+return`⋆⸙ *PHOTOFUNIA LIST* ⸙⋆
+❉▰▰▱▱▣▣▱▱▰▰❉
+✗⃝🕊 ️${prefix}airballoon _text_
+✗⃝🕊 ️${prefix}beachsign _text_
+✗⃝🕊 ️${prefix}blinkinglights _text_
+✗⃝🕊 ️${prefix}bracelet _text_
+✗⃝🕊 ️${prefix}cemeterygates _text_
+✗⃝🕊 ️${prefix}christmas _text_
+✗⃝🕊 ️${prefix}denimembroidery _text_
+✗⃝🕊 ️${prefix}einstein _text_
+✗⃝🕊 ️${prefix}fortunecookie _text_
+✗⃝🕊 ️${prefix}lightgraffiti _text_
+✗⃝🕊 ️${prefix}lightwriting _text_
+✗⃝🕊 ️${prefix}lovelock _text_
+✗⃝🕊 ️${prefix}neonsign _text_
+✗⃝🕊 ️${prefix}nightmare _text_
+✗⃝🕊 ️${prefix}pendant _text_
+✗⃝🕊 ️${prefix}planebanner _text_
+✗⃝🕊 ️${prefix}rugbyball _text_
+✗⃝🕊 ️${prefix}rustywriting _text_
+❉▰▰▱▱▣▣▱▱▰▰❉`
+}
+exports.pfun_list = pfun_list
+
+const oxy_list = (prefix)=> {
+return`⋆⸙ *OXY LIST* ⸙⋆
+❉▰▰▱▱▣▣▱▱▰▰❉
+✗⃝🕊 ️${prefix}3d-glowing _text_
+✗⃝🕊 ️${prefix}3d-rainbow-bg _text_
+✗⃝🕊 ️${prefix}3d-summer _text_
+✗⃝🕊 ️${prefix}3dglowing _text_
+✗⃝🕊 ️${prefix}3dligth _text_
+✗⃝🕊 ️${prefix}3dnature _text_
+✗⃝🕊 ️${prefix}3dsummer _text_
+✗⃝🕊 ️${prefix}8-bit-arcade _text_
+✗⃝🕊 ️${prefix}army-camouflage-fabric _text_
+✗⃝🕊 ️${prefix}avatar _text_
+✗⃝🕊 ️${prefix}banner-metal _text_
+✗⃝🕊 ️${prefix}battlefield-4-rising _text_
+✗⃝🕊 ️${prefix}bcake _text_
+✗⃝🕊 ️${prefix}burn-paper _text_
+✗⃝🕊 ️${prefix}burnpaper _text_
+✗⃝🕊 ️${prefix}butter-fly _text_
+✗⃝🕊 ️${prefix}butterfly _text_
+✗⃝🕊 ️${prefix}camouflage _text_
+✗⃝🕊 ️${prefix}cemetery _text_
+✗⃝🕊 ️${prefix}cname _text_
+✗⃝🕊 ️${prefix}coffecup2 _text_
+✗⃝🕊 ️${prefix}coffee-cup _text_
+✗⃝🕊 ️${prefix}coffeecup _text_
+✗⃝🕊 ️${prefix}crisp _text_
+✗⃝🕊 ️${prefix}crisp-chrome _text_
+✗⃝🕊 ️${prefix}csgo _text_
+✗⃝🕊 ️${prefix}dark-metal _text_
+✗⃝🕊 ️${prefix}embroidery _text_
+✗⃝🕊 ️${prefix}embroidery-text _text_
+✗⃝🕊 ️${prefix}flaming _text_
+✗⃝🕊 ️${prefix}flaming-fire _text_
+✗⃝🕊 ️${prefix}flower _text_
+✗⃝🕊 ️${prefix}flower-hearth _text_
+✗⃝🕊 ️${prefix}flower-typography _text_
+✗⃝🕊 ️${prefix}funny-cup _text_
+✗⃝🕊 ️${prefix}funnycup _text_
+✗⃝🕊 ️${prefix}fur _text_
+✗⃝🕊 ️${prefix}fur-effect _text_
+✗⃝🕊 ️${prefix}glow-rainbow _text_
+✗⃝🕊 ️${prefix}glowneon _text_
+✗⃝🕊 ️${prefix}glowrainbow _text_
+✗⃝🕊 ️${prefix}golden-roses _text_
+✗⃝🕊 ️${prefix}goldenrose _text_
+✗⃝🕊 ️${prefix}gradient-avatar _text_
+✗⃝🕊 ️${prefix}graffiti _text_
+✗⃝🕊 ️${prefix}grafiti-text-cover _text_
+✗⃝🕊 ️${prefix}green-leaves _text_
+✗⃝🕊 ️${prefix}harry _text_
+✗⃝🕊 ️${prefix}harry-poter _text_
+✗⃝🕊 ️${prefix}heart _text_
+✗⃝🕊 ️${prefix}honey _text_
+✗⃝🕊 ️${prefix}honey-text _text_
+✗⃝🕊 ️${prefix}love-messages _text_
+✗⃝🕊 ️${prefix}luxury _text_
+✗⃝🕊 ️${prefix}luxury-royal _text_
+✗⃝🕊 ️${prefix}manga-naruto _text_
+✗⃝🕊 ️${prefix}metalic-gold _text_
+✗⃝🕊 ️${prefix}metallic _text_
+✗⃝🕊 ️${prefix}multi-material _text_
+✗⃝🕊 ️${prefix}nature-3d _text_
+✗⃝🕊 ️${prefix}neon-glow _text_
+✗⃝🕊 ️${prefix}neon-lights _text_
+✗⃝🕊 ️${prefix}nightsky _text_
+✗⃝🕊 ️${prefix}on-the-silk _text_
+✗⃝🕊 ️${prefix}party-neon _text_
+✗⃝🕊 ️${prefix}put-coffee-cup _text_
+✗⃝🕊 ️${prefix}rainbow _text_
+✗⃝🕊 ️${prefix}rainbow-shine _text_
+✗⃝🕊 ️${prefix}romantic _text_
+✗⃝🕊 ️${prefix}romantic-double _text_
+✗⃝🕊 ️${prefix}romantic-messages _text_
+✗⃝🕊 ️${prefix}royal-patterns _text_
+✗⃝🕊 ️${prefix}shadow _text_
+✗⃝🕊 ️${prefix}shadow-sky _text_
+✗⃝🕊 ️${prefix}simple _text_
+✗⃝🕊 ️${prefix}smoke _text_
+✗⃝🕊 ️${prefix}smoke-typography _text_
+✗⃝🕊 ️${prefix}smokyneon _text_
+✗⃝🕊 ️${prefix}striking _text_
+✗⃝🕊 ️${prefix}striking-3d _text_
+✗⃝🕊 ️${prefix}summer-nature-bg _text_
+✗⃝🕊 ️${prefix}summer-watermelon _text_
+✗⃝🕊 ️${prefix}sweet _text_
+✗⃝🕊 ️${prefix}sweet-candy _text_
+✗⃝🕊 ️${prefix}teks-cup _text_
+✗⃝🕊 ️${prefix}text-on-scary _text_
+✗⃝🕊 ️${prefix}tiktok-effect _text_
+✗⃝🕊 ️${prefix}typography _text_
+✗⃝🕊 ️${prefix}typography-leaves _text_
+✗⃝🕊 ️${prefix}under-cube _text_
+✗⃝🕊 ️${prefix}under-fall _text_
+✗⃝🕊 ️${prefix}under-grass _text_
+✗⃝🕊 ️${prefix}under-orchids _text_
+✗⃝🕊 ️${prefix}under-web-matrix _text_
+✗⃝🕊 ️${prefix}underfall _text_
+✗⃝🕊 ️${prefix}underflower _text_
+✗⃝🕊 ️${prefix}underflower2 _text_
+✗⃝🕊 ️${prefix}undergrass _text_
+✗⃝🕊 ️${prefix}underwater _text_
+✗⃝🕊 ️${prefix}vector _text_
+✗⃝🕊 ️${prefix}video-spells-name _text_
+✗⃝🕊 ️${prefix}vintage _text_
+✗⃝🕊 ️${prefix}vintage-text _text_
+✗⃝🕊 ️${prefix}watermelon _text_
+✗⃝🕊 ️${prefix}whitecube _text_
+✗⃝🕊 ️${prefix}wolf-metal _text_
+✗⃝🕊 ️${prefix}wolf_metal _text_
+✗⃝🕊 ️${prefix}wood _text_
+✗⃝🕊 ️${prefix}wood-hearth _text_
+✗⃝🕊 ️${prefix}woodblock _text_
+✗⃝🕊 ️${prefix}woodblock-effect _text_
+✗⃝🕊 ️${prefix}wooden _text_
+✗⃝🕊 ️${prefix}wooden-3d-black _text_
+✗⃝🕊 ️${prefix}wooden-boards _text_
+✗⃝🕊 ️${prefix}write-stars _text_
+❉▰▰▱▱▣▣▱▱▰▰❉`
+}
+exports.oxy_list = oxy_list
+
+const nsfw_list = (prefix)=> {
+return`⋆⸙ *NSFW LIST* ⸙⋆
+❉▰▰▱▱▣▣▱▱▰▰❉
+✗⃝🕊 ️${prefix}anal
+✗⃝🕊 ️${prefix}avatar2
+✗⃝🕊 ️${prefix}bj
+✗⃝🕊 ️${prefix}blowJob
+✗⃝🕊 ️${prefix}boobs
+✗⃝🕊 ️${prefix}classic
+✗⃝🕊 ️${prefix}cumarts
+✗⃝🕊 ️${prefix}cumsluts
+✗⃝🕊 ️${prefix}ero
+✗⃝🕊 ️${prefix}erofeet
+✗⃝🕊 ️${prefix}erokemonomimi
+✗⃝🕊 ️${prefix}erokitsune
+✗⃝🕊 ️${prefix}eroneko
+✗⃝🕊 ️${prefix}eroyuri
+✗⃝🕊 ️${prefix}feet
+✗⃝🕊 ️${prefix}feetgif
+✗⃝🕊 ️${prefix}femdom
+✗⃝🕊 ️${prefix}futanari
+✗⃝🕊 ️${prefix}girl_solo
+✗⃝🕊 ️${prefix}girl_solo_gif
+✗⃝🕊 ️${prefix}hentai
+✗⃝🕊 ️${prefix}hentai_gif
+✗⃝🕊 ️${prefix}holo
+✗⃝🕊 ️${prefix}holoEro
+✗⃝🕊 ️${prefix}kemonomimi
+✗⃝🕊 ️${prefix}keta
+✗⃝🕊 ️${prefix}kitsune
+✗⃝🕊 ️${prefix}kuni
+✗⃝🕊 ️${prefix}lesbian
+✗⃝🕊 ️${prefix}neko
+✗⃝🕊 ️${prefix}neko_gif
+✗⃝🕊 ️${prefix}pussy
+✗⃝🕊 ️${prefix}pussyart
+✗⃝🕊 ️${prefix}pussywankgif
+✗⃝🕊 ️${prefix}spank
+✗⃝🕊 ️${prefix}tits
+✗⃝🕊 ️${prefix}trap
+✗⃝🕊 ️${prefix}yuri
+❉▰▰▱▱▣▣▱▱▰▰❉`
+}
+exports.nsfw_list = nsfw_list
+
