e0aaf35c1c20cabad0b6641c39a5095a01ee6275
Ay Gemuy 24 <91945119+AyGemuy@users.noreply.github.com>
2021-12-18 14:23:03 +0800

    SelfBot
    SelfBot

---
+++ removebg.js
@@ -0,0 +1,32 @@
+
+async function remove_bg (Url) {
+	return new Promise (async (resolve, reject) => {
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const formData = new FormData();
+formData.append('size', 'auto');
+formData.append('image_url', `${Url}`);
+
+axios({
+  method: 'post',
+  url: 'https://api.remove.bg/v1.0/removebg',
+  data: formData,
+  responseType: 'arraybuffer',
+  headers: {
+    ...formData.getHeaders(),
+    'X-Api-Key': 'UBhMUo7FNNdEZ6fmkyAMrAUA',
+  },
+  encoding: null
+})
+.then((response) => {
+  if(response.status != 200) return console.error('Error:', response.status, response.statusText);
+ fs.writeFileSync("no-bg.png", response.data);
+})
+.catch((error) => {
+    return console.error('Request failed:', error);
+});
+});
+}
+
+module.exports = { remove_bg }
